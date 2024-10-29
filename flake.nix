4c4f764b5a1f56e0bc5b6101a92e19b349dc1c21
Lin Xianyi <iynaix@gmail.com>
2024-10-29 18:13:49 +0800

    Add build dependencies

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem =
+        function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forEachSystem (pkgs: {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              # https://devenv.sh/reference/options/
+              dotenv.disableHint = true;
+
+              env = {
+                LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+              };
+
+              packages = with pkgs; [
+                cargo-edit
+                llvmPackages.clang
+                llvmPackages.libclang
+                pkg-config
+                cmake
+                ffmpeg
+                zlib
+              ];
+
+              languages.rust.enable = true;
+            }
+          ];
+        };
+      });
+
+      packages = forEachSystem (pkgs: { });
+    };
+}
